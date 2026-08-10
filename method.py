a5dbe3a83ae2cec0d92f689e236f3ad6d132abca
Bhavanacme <bhavanayarnagula@gmail.com>
2026-08-10 11:42:02 +0530

    first-commit

---
+++ method.py
@@ -0,0 +1,10 @@
+class Addition:
+
+    def addition(self,a,b,c):
+        self.a=a
+        self.b=b
+        self.c=c 
+        print(self.a+self.b+self.c)
+add=Addition()
+add.addition(10,5,15)
+add.addition(20,5,25)    
